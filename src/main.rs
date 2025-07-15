4743df3d29520a1e85cc4393c106ada23e5a33f8
lseeger <lseeger@student.42.fr>
2025-07-15 15:29:59 +0200

    project structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    println!("Enter your name:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("Hello, {}!", input.trim());
 }
