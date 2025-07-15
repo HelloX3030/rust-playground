82008ed475571f51674e066bd1ae6bb07d16fa78
lseeger <lseeger@student.42.fr>
2025-07-15 15:07:22 +0200

    made devcontainer setup

---
+++ target/debug/deps/rust_todo_cli-5d0dc7e458184ca3.d
@@ -0,0 +1,5 @@
+/workspaces/rust-playground/target/debug/deps/librust_todo_cli-5d0dc7e458184ca3.rmeta: src/main.rs
+
+/workspaces/rust-playground/target/debug/deps/rust_todo_cli-5d0dc7e458184ca3.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-todo-cli.d
@@ -0,0 +1,1 @@
+/workspaces/rust-playground/target/debug/rust-todo-cli: /workspaces/rust-playground/src/main.rs
